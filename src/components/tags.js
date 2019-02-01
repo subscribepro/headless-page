c6c420efea1aaf62807215bf3cffba1704f501a9
Orestis Ioannou <oorestisime@gmail.com>
2019-02-01 11:55:43 +0100

    Add clickable tags

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,34 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { navigate } from 'gatsby'
 import styled from 'styled-components'
+import qs from 'qs'
 
 const Wrapper = styled.div`
 ul {
-    margin: 0;
-    padding: 0;
-    li {
-        display: inline;
-        list-style: none;
-    }
+  margin: 0;
+  padding: 0;
+  li {
+    display: inline;
+    list-style: none;
+    cursor: pointer;
+  }
+  li:hover {
+    color: #00baa1;
+  }
 }
 `
 
-const Tags = ({ values, className }) => (
-  <Wrapper className={className}>
-    <ul>
-    {values.map((value, index) => {
-      const isLastItem = index + 1 === values.length
+const Tags = ({ values, className, update }) => {
+  const click = (value) => update
+    ? update(({ filters }) => filters.indexOf(value) === -1 && filters.concat([value]))
+    : navigate(`/?${qs.stringify({ tag: [value] })}`)
 
-      return (
-        <li key={value}>
-          {value}
-          {!isLastItem && <>, </>}
-        </li>
-      )
-    })}
-    </ul>
-  </Wrapper>
-)
+  return (
+    <Wrapper className={className}>
+      <ul>
+      {values.map((value, index) => {
+        const isLastItem = index + 1 === values.length
+
+        return (
+          <li onClick={() => click(value)} key={value}>
+            {value}
+            {!isLastItem && <>, </>}
+          </li>
+        )
+      })}
+      </ul>
+    </Wrapper>
+  )
+}
 
 Tags.propTypes = {
   values: PropTypes.array.isRequired,
